f02d37edac679e5bbf43a7859b10d1b32d236af0
IvSergey <imas.spb1988@gmail.com>
2020-08-11 17:47:44 +0300

    Updates

---
+++ precache-manifest.7cdbb6d2bf2d89968bb312f89fa1f42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a01573190c531125ee1b9d732ae3fc1",
+    "url": "/userForm/index.html"
+  },
+  {
+    "revision": "339d8fe7f993950b81e7",
+    "url": "/userForm/static/css/main.45676170.chunk.css"
+  },
+  {
+    "revision": "5a56dedd238e937656d5",
+    "url": "/userForm/static/js/2.d701dd0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userForm/static/js/2.d701dd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339d8fe7f993950b81e7",
+    "url": "/userForm/static/js/main.f50ebc92.chunk.js"
+  },
+  {
+    "revision": "a94ab1825af6bedf9f87",
+    "url": "/userForm/static/js/runtime-main.b09de19e.js"
+  }
+]);
