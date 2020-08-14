88080f6c05eb1f7a750d56171ed72e35d7af142e
IvSergey <imas.spb1988@gmail.com>
2020-08-14 15:44:44 +0300

    sort commit

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,60 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import {fetch} from "whatwg-fetch";
+import { fetch } from "whatwg-fetch";
 
 const User = ({ token }) => {
     const [resData, setData] = useState([]);
+
+    const [sortData, setSortData] = useState([]);
 
     const [error, setError] = useState("");
 
     const [value, setValue] = useState("");
 
-    async function postData(url = '') {
-        const response = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Token ${token}`
-            }
-        });
-        const r = await response.json();
-        console.log(r)
-        return r
-
-    }
+    const [sortUp, setSortUp] = useState(true);
 
     useEffect(() => {
-        
-        const res = postData('https://emphasoft-test-assignment.herokuapp.com/api/v1/users/')
+        async function postData(url = '') {
+            const response = await fetch(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`
+                }
+            });
+            const r = await response.json();
+            console.log(r)
+            return r
+
+        }
+
+
+
+        postData('https://emphasoft-test-assignment.herokuapp.com/api/v1/users/')
             .then(data => {
-                return setData(data)
-            }).catch(e => setError(e.message));
+                setData(data);
+                setSortData(data);
+            })
+            .catch((e) => console.error(e.message));
     }, [token]);
 
+
+    const sortDataUp = (data) => data.sort((a, b) => a.id - b.id);
+    const sortDataDown = (data) => data.sort((a, b) => b.id - a.id);
+    const handleSort = () => {
+        const data = sortUp ? sortDataUp(sortData) : sortDataDown(sortData);
+        setSortData(data);
+        setSortUp(!sortUp);
+    };
+
+
+    const handleChangeName = (e) => {
+        const res = resData.filter((u) => u.username.toLowerCase().includes(e.target.value.toLowerCase())
+        );
     
+    setSortData(res);
+    setValue(e.target.value)
+}
 
-    const handleChangeName = (e) =>{
-        setValue(e.target.value)
-    }
-        
-    const searchData = resData.length && resData.filter(u => u.username.toLowerCase().includes(value))
-    const data = value ? searchData : resData
-    console.log(data,"data")
 
-    return (
-        <div className="User">
-            <input onChange={(e)=>handleChangeName(e)}/>
-            <ul>
-                {
-                    data.sort((a, b) => a.id - b.id).map(user => {
-                        return (
-                            <li key={user.id}>
-                                <span>User Name: {user.username}</span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    );
+// const searchData = resData.length && resData.filter(u => u.username.toLowerCase().includes(value.toLowerCase()))
+// const data = value ? searchData : resData
+// console.log(data, "data")
+
+
+
+return (
+    <div className="User">
+        <input onChange={(e) => handleChangeName(e)} />
+        <button onClick={handleSort}>SortID</button>
+        <ul>
+            {
+                sortData.map(user => {
+                    return (
+                        <li key={user.id}>
+                            <span>UserName: {user.username} userID: {user.id}</span>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    </div>
+);
 }
 
 export default User;
